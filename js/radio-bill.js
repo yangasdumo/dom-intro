3831ebfd2d6e72d6fc63962a2ccbe721b28816ca
Yanga <yangasiqiki@gmail.com>
2022-05-09 16:52:55 +0200

    changes

--- js/radio-bill.js
+++ js/radio-bill.js
@@ -1,8 +1,17 @@
 // get a reference to the sms or call radio buttons
-
+const textBoxElement = document.querySelector(".billTypeText");
+const callsTotalElem = document.querySelector(".callTotalOne");
+const smsTotalElem = document.querySelector(".smsTotalOne");
+const totalCostElem = document.querySelector(".totalOne");
 //get a reference to the add button
+const textTotalAddBtn = document.querySelector(".addToBillBtn");
+//create a variable that will keep trac var callsTotal = 0;
+ var callsTotal = 0;
+var smsTotal = 0;
+var TotalCost =0;
+// these variables are global and defined outside of the Add button event listener.
 
-//create a variable that will keep track of the total bill
+// the event listener is declared below here
 
 //add an event listener for when the add button is pressed
 
@@ -10,3 +19,7 @@
 // * add the appropriate value to the running total
 // * add nothing for invalid values that is not 'call' or 'sms'.
 // * display the latest total on the screen
+ 
+
+
+textTotalAddBtn.addEventListener('click', textBillTotal);
