2f54df36650b2aaa2bfd2a1efb26b58a9b464e4b
Yanga <yangasiqiki@gmail.com>
2022-05-09 11:43:53 +0200

    fist commit

--- js/calculate-bill.js
+++ js/calculate-bill.js
@@ -1,13 +1,52 @@
-//get a reference to the calculate button
 
+const calculateBtnElement = document.querySelector(".calculateBtn");
 //get a reference to the billTotal element
-
+const billTotalElement = document.querySelector(".billTotal");
 //get a reference to the billString
+const billStringElement = document.querySelector(".billString");
 
-//create the function that will be called when the calculate button is pressed
-//  * this function should read the string value entered - split it on a comma.
-//  * loop over all the entries in the the resulting list
-//  * check if it is a call or an sms and add the right amount to the overall total
-//  * once done looping over all the entries - display the total onto the screen in the billTotal element
+function totalPhoneBill(total) {
+    // logic goes herefunction totalPhoneBill(total){
+    var callList = total.split(',')
+    var call = []
+    var sms = []
 
-//link the function to a click event on the calculate button
+    for (var i = 0; i < callList.length; i++) {
+        let newCallList = callList[i].trim();
+
+        if (newCallList.includes('SMS')) {
+            sms.push(newCallList)
+        } else if
+            (newCallList.includes("CALL")){
+            call.push(newCallList)
+        }
+    }
+
+
+    var totalCall = call.length;
+    var totalSms = sms.length;
+    var a = totalCall * 2.75
+    var b = totalSms * 0.75
+    var c = a + b
+    return c.toFixed(2)
+}
+function calculateBtnClicked() {
+
+    // get the string entered in the textArea
+    var billString = billStringElement.value;
+    var validateCaps = billString.toUpperCase()
+    //round to two decimals
+    var roundedBillTotal = totalPhoneBill(validateCaps);
+    billTotalElement.innerHTML = roundedBillTotal;
+
+    if (roundedBillTotal > 30) {
+        billTotalElement.classList.remove("warning");
+        billTotalElement.classList.add("danger");
+    }
+
+    else if (roundedBillTotal > 20) {
+        billTotalElement.classList.add("warning")
+    }
+}
+
+calculateBtnElement.addEventListener('click', calculateBtnClicked);
