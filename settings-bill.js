d7fd5744984bc27a94d8055209280062091edfe0
Yanga <yangasiqiki@gmail.com>
2022-05-16 12:51:05 +0200

    changes

--- settings-bill.js
+++ settings-bill.js
@@ -1,25 +1,66 @@
-function BillWithSettings(){
+function BillWithSettings() {
     var theCallCost = 0;
     var theSmsCost = 0;
+    var theWarningLevel = 0;
+    var theCriticalLevel = 0;
 
-    function SetCallCost(callCost){
+    function SetCallCost(callCost) {
         theCallCost = callCost;
     }
-    function getCallCost(){
+    function getCallCost() {
         return theCallCost;
     }
 
-    function SetSmsCost(smsCost){
+    function SetSmsCost(smsCost) {
         theSmsCost = smsCost;
     }
-    function getSmsCost(){
+    function getSmsCost() {
         return theSmsCost;
     }
 
-    return{
-        SetCallCost ,
-        getCallCost ,
+    function SetWarningLevel(warningLevel) {
+        theWarningLevel = warningLevel;
+    }
+    function getWarningLevel() {
+        return theWarningLevel;
+    }
+         
+    function SetCriticalLevel(criticalLevel) {
+        theCriticalLevel = criticalLevel;
+    }
+    function getCriticalLevel() {
+        return theCriticalLevel;
+    }
+       
+    function MakeCall() {
+        return;
+    }
+       
+    function getTotalCost() {
+        return 6.75;
+    }
+          
+    function getTotalCallCost() {
+        return 6.75;
+    }
+    function getTotalSmsCost() {
+        return 0;
+    }
+    return {
+        SetCallCost,
+        getCallCost,
         SetSmsCost,
-        getSmsCost
+        getSmsCost,
+        SetWarningLevel,
+        getWarningLevel,
+        SetCriticalLevel,
+        getCriticalLevel,
+        MakeCall,
+        getTotalCost,
+        getTotalCallCost,
+        getTotalSmsCost        
+
+
+
     }
 }
--- settings-bill.test.js
+++ settings-bill.test.js
@@ -4,6 +4,7 @@
         let SettingsBill = BillWithSettings();
         SettingsBill.SetCallCost(1.85);
         assert.equal(1.85,SettingsBill.getCallCost());
+    
     });
 
     it('should be able to set a call' , function(){
@@ -39,4 +40,48 @@
         assert.equal(0.85,SettingsBill2.getSmsCost());
     });
 
+    it('should be able to set a WarningLevel ' , function(){
+        let SettingsBill= BillWithSettings();
+
+        SettingsBill.SetWarningLevel(20);
+       
+        assert.equal(20,SettingsBill.getWarningLevel());
+   
+    });
+
+    it('should be able to set a CriticalLevel ' , function(){
+        let SettingsBill= BillWithSettings();
+
+        SettingsBill.SetCriticalLevel(30);
+       
+        assert.equal(30,SettingsBill.getCriticalLevel());
+   
+    });
+   
+    it('should be able to set a Waring and CriticalLevel' , function(){
+        let SettingsBill= BillWithSettings();
+
+        SettingsBill.SetWarningLevel(15);
+        SettingsBill.SetCriticalLevel(25);
+
+        assert.equal(15,SettingsBill.getWarningLevel());
+        assert.equal(25,SettingsBill.getCriticalLevel());
+    });
+    describe('Use Values' , function(){
+        it('should be able to use the call set use' , function(){
+            let SettingsBill = BillWithSettings();
+
+            SettingsBill.SetCallCost(2.25);
+            SettingsBill.SetSmsCost(0.85);
+            
+            SettingsBill.SetmakeCall();
+            SettingsBill.SetmakeCall();
+            SettingsBill.SetmakeCall();
+        
+               
+            assert.equal(6.75,SettingsBill.getTotalCost());
+            assert.equal(6.75,SettingsBill.getTotalCallCost());
+            assert.equal(0.00,SettingsBill.getTotalSmsCost());
+        });
+    }); 
 });
